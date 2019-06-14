0d21167c93286f07fec486f5ab9cf971b62e30ba
William Nyström <wille.nystrom@gmail.com>
2019-06-14 16:28:10 +0200

    initial commit

---
+++ weather.js
@@ -0,0 +1,91 @@
+const API_KEY = '92323150727077c1b657e697a0b8d525'
+const API_VERSION = '2.5'
+const cities = {
+	Stockholm: 2673730,
+	Chaguana: 7521937
+}
+
+function getWeather(city) {
+	return fetch(`https://api.openweathermap.org/data/${API_VERSION}/weather?id=${city}&units=metric&APPID=${API_KEY}`)
+		.then(response => response.json())
+}
+
+async function refresh() {
+	let temp1, temp2
+	const updated = document.getElementById('updated-time')
+
+	const city1 = {
+		name: document.getElementById('city1-name'),
+		temp: document.getElementById('city1-temp')
+	}
+
+	const city2 = {
+		name: document.getElementById('city2-name'),
+		temp: document.getElementById('city2-temp')
+	}
+
+	const answer = document.getElementById('answer')
+
+	await getWeather(cities.Stockholm).then(data => {
+		city1.name.innerText = data.name
+		city1.temp.innerText = data.main.temp
+
+		temp1 = data.main.temp
+	})
+
+	await getWeather(cities.Chaguana).then(data => {
+		city2.name.innerText = data.name
+		city2.temp.innerText = data.main.temp
+
+		temp2 = data.main.temp
+	})
+
+	const tempDiff = Math.abs(temp1 - temp2)
+
+	if (tempDiff < 5) {
+		if (tempDiff < 3) {
+			answer.innerText = 'Ja'
+
+			notify();
+		} else {
+			answer.innerText = 'Typ'
+		}
+	} else {
+		answer.innerText = 'Nej'
+	}
+
+	const time = new Date()
+
+	updated.innerText = time.toLocaleString()
+	updated.setAttribute('datetime', time.toISOString())
+}
+
+function notify() {
+	// Let's check if the browser supports notifications
+	if (!("Notification" in window)) {
+		alert("This browser does not support system notifications");
+	}
+
+	// Let's check whether notification permissions have already been granted
+	else if (Notification.permission === "granted") {
+		// If it's okay let's create a notification
+		var notification = new Notification("Det är hett som i Trinidad!");
+	}
+
+	// Otherwise, we need to ask the user for permission
+	else if (Notification.permission !== 'denied') {
+		Notification.requestPermission(function(permission) {
+			// If the user accepts, let's create a notification
+			if (permission === "granted") {
+				var notification = new Notification("Det är hett som i Trinidad!");
+			}
+		});
+	}
+
+  // Finally, if the user has denied notifications and you 
+  // want to be respectful there is no need to bother them any more.
+}
+
+window.onload = refresh
+
+setInterval(refresh, 1000 * 60 * 10) //refresh every 10 minutes
